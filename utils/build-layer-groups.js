6e29673516647002ec6a86ff3002f2526fc306c0
Chris Whong <chris@whong.org>
2018-08-06 18:18:25 -0400

    append layergroup layers to base style

--- utils/build-layer-groups.js
+++ utils/build-layer-groups.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fetch = require('node-fetch');
 const loadJsonFile = require('load-json-file');
 
 const buildLayerGroups = async (config) => {
@@ -11,17 +12,20 @@ const buildLayerGroups = async (config) => {
     const promises = layerGroups.map(layerGroup => loadJsonFile(path.resolve(__dirname, `../layer-groups/${layerGroup}.json`)));
     const layerGroupConfigs = await Promise.all(promises);
 
+    const baseStyle = await fetch('https://raw.githubusercontent.com/NYCPlanning/labs-gl-style/master/data/style.json')
+      .then(d => d.json());
+
     let layers = [];
-
-
 
     layerGroupConfigs.forEach((config) => {
       const internalLayers = config.layers.map(d => d.layer);
       layers = [...layers, ...internalLayers]
     })
 
+    baseStyle.layers = [...baseStyle.layers, ...layers]
+
     resolve({
-      meta: layers
+      meta: baseStyle
     })
   })
 }
