c63d1705bc450fc0440d365ff2d680340f9ec2b3
Chris Whong <chris@whong.org>
2018-08-06 21:40:31 -0400

    add some sources

---
+++ sources/zoning-districts.js
@@ -0,0 +1,17 @@
+module.exports = {
+  id: 'zoning-districts',
+  type: 'cartovector',
+  'source-layers': [
+    {
+      id: 'zoning-districts',
+      sql: `SELECT * FROM (
+              SELECT *, CASE
+                WHEN SUBSTRING(zonedist, 3, 1) = '-' THEN LEFT(zonedist, 2)
+                WHEN SUBSTRING(zonedist, 3, 1) ~ E'[A-Z]' THEN LEFT(zonedist, 2)
+                WHEN SUBSTRING(zonedist, 3, 1) ~ E'[0-9]' THEN LEFT(zonedist, 3)
+                ELSE zonedist
+              END as primaryzone FROM zoning_districts_v201806
+            ) a`,
+    },
+  ],
+}
