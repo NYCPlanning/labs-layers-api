b75aeeb090b3ba65d8122e51aad1e3ff4718e598
allthesignals <wilburnforce@gmail.com>
2018-08-10 16:03:09 -0400

    Source and layer-group validations in tests

---
+++ test/validations/data.js
@@ -0,0 +1,82 @@
+const chai = require('chai');
+const Joi = require('joi');
+const path = require('path');
+const fs = require('fs');
+
+const { find } = require('../../utils/local-resources-utilities');
+
+
+const getFilenames = resourceName => fs.readdirSync(path.resolve(__dirname, `../../data/${resourceName}`));
+
+const should = chai.should();
+
+const layerGroupSchema = Joi.object().keys({
+  id: Joi.string().required(),
+  title: Joi.string(),
+  visible: Joi.boolean(),
+  titleTooltip: Joi.string(),
+  meta: Joi.object(),
+  layers: Joi.array().items(
+    Joi.object({
+      style: Joi.object().required(),
+      displayName: Joi.string(),
+      before: Joi.string(),
+      clickable: Joi.boolean(),
+      highlightable: Joi.boolean(),
+      tooltipable: Joi.boolean(),
+      tooltipTemplate: Joi.string(),
+    }),
+  ),
+  legendConfig: Joi.object(),
+  legendIcon: Joi.string(),
+  legendColor: Joi.string(),
+});
+
+const sourceSchema = Joi.object().keys({
+  id: Joi.string().required(),
+  type: Joi.valid(
+    'vector',
+    'raster',
+    'geojson',
+    'cartovector',
+  ),
+  minzoom: Joi.number(),
+  maxzoom: Joi.number(),
+  'source-layers': Joi.array().items(Joi.object())
+    .when('type', {
+      is: 'cartovector',
+      then: Joi.required(),
+    }),
+  tiles: Joi.array().items(Joi.string()).when('type', {
+    is: 'raster',
+    then: Joi.required(),
+  }),
+  tileSize: Joi.number().when('type', {
+    is: 'raster',
+    then: Joi.required(),
+  }),
+});
+
+function testLayerGroup(filename) {
+  it(`validates layer-groups/${filename}`, async () => {
+    const layerGroup = await find('layer-groups', filename.split('.')[0]);
+    const { error } = layerGroupSchema.validate(layerGroup);
+    should.not.exist(error);
+  });
+}
+
+function testSource(filename) {
+  it(`validates sources/${filename}`, async () => {
+    const source = await find('sources', filename.split('.')[0]);
+    const { error } = sourceSchema.validate(source);
+    should.not.exist(error);
+  });
+}
+
+describe('Data validations', async () => {
+  const layerGroupFilenames = getFilenames('layer-groups');
+  const sourceFilenames = getFilenames('sources');
+
+  layerGroupFilenames.forEach(filename => testLayerGroup(filename));
+  sourceFilenames.forEach(filename => testSource(filename));
+});
--- utils/local-resources-utilities/find-all.js
+++ utils/local-resources-utilities/find-all.js
@@ -1,7 +1,17 @@
-// findOne(type, id)
 const path = require('path');
 const loadJsonFile = require('load-json-file');
+const fs = require('fs');
 
-module.exports = resourceName => loadJsonFile(
-  path.resolve(__dirname, `../../data/${resourceName}`),
-);
+module.exports = (resourceName) => {
+  const fileNames = fs.readdirSync(
+    path.resolve(__dirname, `../../data/${resourceName}`),
+  );
+
+  return Promise.all(
+    fileNames.map(
+      fileName => loadJsonFile(
+        path.resolve(__dirname, `../../data/${resourceName}/${fileName}`),
+      ),
+    ),
+  );
+};
