141c3fcff28b4162968381f61ebf26a8439dfd3e
Andy Cochran <acochran@planning.nyc.gov>
2018-08-27 17:13:36 -0400

    refactor the schema for legends

--- schemas/layer-group.js
+++ schemas/layer-group.js
@@ -1,12 +1,28 @@
 const Joi = require('joi');
 const layerSchema = require('./layer');
 
+const legendItemSchema = Joi.object().keys({
+  label: Joi.string(),
+  icon: Joi.object().keys({
+    type: Joi.string(),
+    options: Joi.object(),
+  }),
+  tooltip: Joi.string(),
+});
+
 module.exports = Joi.object().keys({
   id: Joi.string().required(),
-  title: Joi.string(),
   visible: Joi.boolean(),
+  legend: Joi.object().keys({
+    label: Joi.string(),
+    icon: Joi.string(),
+    tooltip: Joi.string(),
+    items: Joi.array().items(
+      legendItemSchema,
+    ),
+  }),
   layerVisibilityType: Joi.string(),
-  titleTooltip: Joi.string(),
+
   meta: Joi.object(),
   layers: Joi.array().items(
     layerSchema,
