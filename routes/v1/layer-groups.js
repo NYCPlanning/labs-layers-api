0417c5abe8069a349ff68e9cb87ff7c0ff622f87
allthesignals <wilburnforce@gmail.com>
2018-09-10 16:58:06 -0400

    Serialize sources by sideloading them with layer-groups

--- routes/v1/layer-groups.js
+++ routes/v1/layer-groups.js
@@ -26,8 +26,10 @@ router.get('/', async (ctx) => {
     data = await findAll('layer-groups');
   }
 
+  const sources = await findAll('sources');
+
   ctx.body = {
-    ...layerGroupSerializer(data),
+    ...layerGroupSerializer(data, sources),
     meta: {
       mapboxStyle: await buildMapboxStyle(data),
     },
--- serializers/layer-groups.js
+++ serializers/layer-groups.js
@@ -1,17 +1,36 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 const layerGroupSchema = require('../schemas/layer-group');
 const layerSchema = require('../schemas/layer');
+const sourceSchema = require('../schemas/source');
 
 const { children: layerGroupAttrs } = layerGroupSchema.describe();
 const { children: layerAttrs } = layerSchema.describe();
+const { children: sourceAttrs } = sourceSchema.describe();
 
-module.exports = data => new JSONAPISerializer('layer-groups', {
-  attributes: Object.keys(layerGroupAttrs),
+module.exports = (data, sources = []) => new JSONAPISerializer('layer-groups', {
+  transform(record) {
+    const mutatedRecord = record;
+    mutatedRecord.sources = sources
+      .filter(
+        sourceObject => record.layers
+          .map(({ style: { source } }) => source).includes(sourceObject.id),
+      );
+
+    return mutatedRecord;
+  },
+  attributes: Object.keys(layerGroupAttrs).concat('sources'),
   layers: {
     ref(parent, child) {
       return child.style.id;
     },
     included: true,
     attributes: Object.keys(layerAttrs),
+  },
+  sources: {
+    ref(parent, child) {
+      return child.id;
+    },
+    included: true,
+    attributes: Object.keys(sourceAttrs),
   },
 }).serialize(data);
