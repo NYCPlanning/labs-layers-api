e5a4e429b6e2eb2857fe5aaaabef4e58c2285d09
Jonathan Pichot <jpichot@planning.nyc.gov>
2019-07-05 14:02:25 -0400

    Address vulnerability; add Sentry and New Relic (#152)

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// 
+require('newrelic');
+
 const Koa = require('koa');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
@@ -6,6 +9,16 @@ const serve = require('koa-static');
 const v1Routes = require('./routes/v1');
 
 const app = new Koa();
+
+// Configure Sentry
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+app.on('error', (err, ctx) => {
+  Sentry.withScope(scope => {
+    scope.addEventProcessor(event => Sentry.Handlers.parseRequest(event, ctx.request));
+    Sentry.captureException(err);
+  });
+});
 
 app.use(bodyParser());
 app.use(cors({
